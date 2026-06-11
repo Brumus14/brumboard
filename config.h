1e33c554ccc0c9ecc281e58241f0ff3c1648bc66
Brumus14 <luke.barkess@icloud.com>
2026-06-11 14:26:58 +0100

    Adding oneshot shift

--- config.h
+++ config.h
@@ -25,6 +25,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define TAPPING_TERM 200
 #define QUICK_TAP_TERM 0
 #define PERMISSIVE_HOLD
+#define ONESHOT_TIMEOUT 5000
 // #define COMBO_TERM 40
 // #define RETRO_TAPPING
 
